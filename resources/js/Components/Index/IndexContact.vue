0048aa8a11653ba34dfbb9c49a0a2ecfbdb5256c
trephy <trephyyy@gmail.com>
2025-05-09 00:20:08 +0300

    Refactor layout header for improved scroll handling and responsiveness

--- resources/js/Components/Index/IndexContact.vue
+++ resources/js/Components/Index/IndexContact.vue
@@ -1,7 +1,6 @@
 <template>
-    <section
-    id="contact"
-    class="relative z-20 flex flex-col items-center justify-center min-h-screen md:flex-row bg-gradient-to-r from-primary-500 to-primary-300">
+    <section id="contact"
+        class="relative z-20 flex flex-col items-center justify-center min-h-screen md:flex-row bg-gradient-to-r from-primary-500 to-primary-300">
         <div class="absolute w-full h-full">
             <GridBackground />
         </div>
@@ -9,52 +8,40 @@
             <h1 class="mb-4 text-4xl font-bold">{{ t("contact.title") }}</h1>
             <p class="mb-6 text-lg">
                 {{ t("contact.description") }}
-                <div class="divider"></div>
-                {{ t("contact.socialMedia") }}
+            <div class="divider"></div>
+            {{ t("contact.socialMedia") }}
             </p>
             <div class="flex space-x-4">
-                <a v-for="social in socials" :href="social.link" >
+                <a v-for="social in socials" :href="social.link">
                     <Icon :icon="social.icon" class="text-2xl" />
                 </a>
-              
+
             </div>
         </div>
+        <a rel="me" class="hidden" href="https://mastodon.social/@trephy">Mastodon</a>
         <div class="z-10 w-full p-6 md:w-1/2">
             <form @submit.prevent="submitForm" class="space-y-4">
                 <div>
-                    <label for="name" class="block text-sm font-medium text-muted-200">{{ t("contact.form.name") }}</label>
-                    <input
-                        type="text"
-                        id="name"
-                        v-model="form.name"
-                        class="block w-full p-2 mt-1 text-black bg-white border border-gray-200 rounded-md"
-                        required
-                    />
+                    <label for="name" class="block text-sm font-medium text-muted-200">{{ t("contact.form.name")
+                        }}</label>
+                    <input type="text" id="name" v-model="form.name"
+                        class="block w-full p-2 mt-1 text-black bg-white border border-gray-200 rounded-md" required />
                 </div>
                 <div>
-                    <label for="email" class="block text-sm font-medium text-muted-200">{{ t("contact.form.email") }}</label>
-                    <input
-                        type="email"
-                        id="email"
-                        v-model="form.email"
-                        class="block w-full p-2 mt-1 text-black bg-white border border-gray-200 rounded-md"
-                        required
-                    />
+                    <label for="email" class="block text-sm font-medium text-muted-200">{{ t("contact.form.email")
+                        }}</label>
+                    <input type="email" id="email" v-model="form.email"
+                        class="block w-full p-2 mt-1 text-black bg-white border border-gray-200 rounded-md" required />
                 </div>
                 <div>
-                    <label for="message" class="block text-sm font-medium text-muted-200">{{ t("contact.form.message") }}</label>
-                    <textarea
-                        id="message"
-                        v-model="form.message"
-                        class="block w-full p-2 mt-1 text-black bg-white border border-gray-200 rounded-md"
-                        rows="4"
-                        required
-                    ></textarea>
+                    <label for="message" class="block text-sm font-medium text-muted-200">{{ t("contact.form.message")
+                        }}</label>
+                    <textarea id="message" v-model="form.message"
+                        class="block w-full p-2 mt-1 text-black bg-white border border-gray-200 rounded-md" rows="4"
+                        required></textarea>
                 </div>
-                <button
-                    type="submit"
-                    class="w-full px-4 py-2 font-semibold text-white rounded-md bg-secondary-500 hover:bg-secondary-600"
-                >
+                <button type="submit"
+                    class="w-full px-4 py-2 font-semibold text-white rounded-md bg-secondary-500 hover:bg-secondary-600">
                     {{ t("contact.form.submit") }}
                 </button>
             </form>
@@ -85,16 +72,16 @@ const submitForm = () => {
         email: form.value.email,
         message: form.value.message,
     })
-    .then(response => {
-        console.log("Message sent successfully:", response.data);
-        form.value.name = "";
-        form.value.email = "";
-        form.value.message = "";
-    })
-    .catch(error => {
-        console.error("Error sending message:", error);
-        alert("There was an error sending your message. Please try again.");
-    });
+        .then(response => {
+            console.log("Message sent successfully:", response.data);
+            form.value.name = "";
+            form.value.email = "";
+            form.value.message = "";
+        })
+        .catch(error => {
+            console.error("Error sending message:", error);
+            alert("There was an error sending your message. Please try again.");
+        });
 
     anime({
         targets: form.value,
--- resources/js/Components/layout/LayoutHeader.vue
+++ resources/js/Components/layout/LayoutHeader.vue
@@ -1,35 +1,46 @@
 <script setup>
-import { ref } from "vue";
+import { ref, onMounted, onBeforeUnmount } from "vue";
 import { Link } from "@inertiajs/vue3";
 import { useI18n } from "vue-i18n";
 import { Icon } from "@iconify/vue";
 
 const { t, locale } = useI18n();
 const isOpen = ref(false);
+const isScrolled = ref(false);
 
 const changeLanguage = () => {
     locale.value = locale.value === "en" ? "bg" : "en";
 };
+
+// Handle scroll events
+const handleScroll = () => {
+    // Change header style when page is scrolled more than 20px
+    isScrolled.value = window.scrollY > 20;
+};
+
+// Add scroll event listener when component is mounted
+onMounted(() => {
+    window.addEventListener('scroll', handleScroll);
+});
+
+// Remove event listener when component is unmounted
+onBeforeUnmount(() => {
+    window.removeEventListener('scroll', handleScroll);
+});
 </script>
 
 <template>
-    <header
-        class="z-30 w-[90%] mx-[5%] flex fixed top-0 my-3 bg-[#ffffff80] rounded-xl navbar text-secondary-100 transition-all duration-300 animate-slide-down"
-    >
+    <header :class="[
+        'z-30 w-[90%] mx-[5%] flex fixed top-0 my-3 bg-[#ffffff80] navbar text-secondary-100 transition-all duration-300 animate-slide-down',
+        isScrolled ? 'rounded-none shadow-md w-screen bg-[#cacaca] mx-0 my-0' : 'rounded-xl'
+    ]">
         <div class="relative flex items-center justify-between w-full px-10">
-            <Link
-                href="/"
-                class="text-2xl font-black md:text-3xl btn btn-ghost"
-            >
-                {{ t("header.brand") }}
+            <Link href="/" class="text-2xl font-black md:text-3xl btn btn-ghost">
+            {{ t("header.brand") }}
             </Link>
-            <div
-                :class="{ block: isOpen, hidden: !isOpen }"
-                class="w-full text-right transition-all duration-300 md:flex md:items-center md:justify-end md:w-auto"
-            >
-                <ul
-                    class="flex flex-col p-0 text-lg font-semibold md:text-xl menu menu-horizontal md:flex-row"
-                >
+            <div :class="{ block: isOpen, hidden: !isOpen }"
+                class="w-full text-right transition-all duration-300 md:flex md:items-center md:justify-end md:w-auto">
+                <ul class="flex flex-col p-0 text-lg font-semibold md:text-xl menu menu-horizontal md:flex-row">
                     <li>
                         <a href="#" class="scroll-smooth">{{
                             t("header.nav.home")
@@ -51,21 +62,14 @@ const changeLanguage = () => {
                         }}</a>
                     </li>
                 </ul>
-                <button
-                    @click="changeLanguage"
-                    class="ml-4 text-lg font-semibold text-secondary-100"
-                >
+                <button @click="changeLanguage" class="ml-4 text-lg font-semibold text-secondary-100">
                     {{ locale === "en" ? "🇧🇬" : "🇬🇧" }}
                 </button>
             </div>
             <button
                 class="relative left-0 block ml-auto text-3xl transition-transform duration-300 md:hidden focus:outline-none"
-                @click="isOpen = !isOpen"
-            >
-                <Icon
-                    :icon="isOpen ? 'bi:x' : 'bi:justify'"
-                    class="text-secondary-100"
-                />
+                @click="isOpen = !isOpen">
+                <Icon :icon="isOpen ? 'bi:x' : 'bi:justify'" class="text-secondary-100" />
             </button>
         </div>
     </header>
@@ -81,6 +85,7 @@ const changeLanguage = () => {
         transform: translateY(-20px);
         opacity: 0;
     }
+
     to {
         transform: translateY(0);
         opacity: 1;
